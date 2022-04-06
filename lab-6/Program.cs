1261f0d7d9684243a784313bb38e962101e9c022
kacper.siciarz <kacpersiciarz2001@gmail.com>
2022-04-06 14:40:56 +0200

    Baza klasy Priority Queue

--- lab-6/Program.cs
+++ lab-6/Program.cs
@@ -6,22 +6,20 @@ namespace lab_6
     {
         static void Main(string[] args)
         {
+            // Queue
             IntQueue queue = new IntQueue(3);
             queue.Add(4);
             queue.Add(6);
             queue.Add(10);
             if (queue.Count() == 3)
-            {
                 Console.WriteLine("1");
-            }
             if (queue.Remove() == 4)
-            {
                 Console.WriteLine("2");
-            }
             if (queue.Count() == 2)
-            {
                 Console.WriteLine("3");
-            }
+
+            // Priority Queue
+            PriorityQueue priorityQueue = new PriorityQueue(5);
 
         }
     }
@@ -52,9 +50,10 @@ namespace lab_6
             if (!IsEmpty())
             {
                 int removed = array[0];
-                for (int i = 0; i < Count()-1; i++)
+                // moving all values back by 1
+                for (int i = 0; i < Count() - 1; i++)
                 {
-                    array[i] = array[i+1];
+                    array[i] = array[i + 1];
                 }
                 index--;
                 return removed;
@@ -75,35 +74,75 @@ namespace lab_6
         }
     }
 
-    //class priorityQueue
-    //{
-    //    private int[] array;
-    //    private int lastIndex;
+    class PriorityQueue
+    {
+        private int[] array;
+        private int last = -1;
 
-    //    public priorityQueue()
-    //    {
+        public PriorityQueue(int size)
+        {
+            array = new int[size];
+        }
 
-    //    }
+        public bool Insert(int value)
+        {
+            if (!IsFull())
+            {
+                array[++last] = value;
+                RebuildUp(last);
+                return true;
+            }
+            return false;
+        }
+        public int Remove()
+        {
+            return 0;
+        }
+        public int Count()
+        {
+            return last + 1;
+        }
+        public bool IsEmpty()
+        {
+            return Count() == 0;
+        }
+        public bool IsFull()
+        {
+            return Count() == array.Length;
+        }
 
-    //    public bool Insert(int value)
-    //    {
+        private int leftChild(int parent)
+        {
+            return parent * 2 + 1;
+        }
+        private int rightChild(int parent)
+        {
+            return parent * 2 + 2;
+        }
+        private int Parent(int child)
+        {
+            return (child - 1) / 2;
+        }
+        private void RebuildUp(int node)
+        {
+            // loopig throught the binary tree
+            while (node > 0)
+            {
+                int nodeValue = array[node];
+                int parentValue = array[Parent(node)];
 
-    //    }
-    //    public int Remove()
-    //    {
+                // switching values
+                if (nodeValue > parentValue)
+                {
+                    array[Parent(node)] = nodeValue;
+                    array[node] = parentValue;
+                    // (array[Parent(node)], array[node]) = (array[node], array[Parent(node)]); // same thing
 
-    //    }
-    //    public int Count()
-    //    {
-    //        return count;
-    //    }
-    //    public bool IsEmpty()
-    //    {
-    //        return Count() == 0;
-    //    }
-    //    public bool IsFull()
-    //    {
-    //        return Count() == array.Length;
-    //    }
-    //}
+                    node = Parent(node);
+                }
+                else
+                    break;
+            }
+        }
+    }
 }
